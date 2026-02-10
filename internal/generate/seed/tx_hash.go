f8b12a40bcc26c3b7d97f1c31204ea30ebc8c432
Christian Jonas <christianjonas99@gmail.com>
2026-02-10 04:03:04 -0500

    add seed data stream writer

--- internal/generate/seed/tx_hash.go
+++ internal/generate/seed/tx_hash.go
@@ -2,13 +2,11 @@ package seed
 
 import (
 	"context"
-	"encoding/json"
-	"os"
 
 	"github.com/pkg/errors"
 
 	"github.com/stellar/go-stellar-sdk/clients/rpcclient"
-	"github.com/stellar/go-stellar-sdk/protocols/rpc"
+	protocol "github.com/stellar/go-stellar-sdk/protocols/rpc"
 )
 
 var (
@@ -23,48 +21,41 @@ type TxData struct {
 	Success bool   `json:"success"`
 }
 
-type Transactions struct {
-	Transactions []TxData `json:"txs"`
-}
-
 func SeedTxHashData(
 	ctx context.Context,
 	rpcClient *rpcclient.Client,
+	writer *SeedWriter,
 	parameters PreseedParameters,
 ) error {
-	transactions := []TxData{}
-	limit := min(TxLimit, parameters.MaxLedger-parameters.MinLedger+1)
+	if err := writer.StartArray("txs"); err != nil {
+		return errors.Wrap(err, "failed to start txs array")
+	}
 
-	for ; parameters.MinLedger < parameters.MaxLedger; parameters.MinLedger += TxLimit {
+	limit := min(TxLimit, parameters.Range.Last-parameters.Range.First+1)
+
+	for ; parameters.Range.First < parameters.Range.Last; parameters.Range.First += TxLimit {
 		req := protocol.GetTransactionsRequest{
-			StartLedger: parameters.MinLedger,
+			StartLedger: parameters.Range.First,
 			Pagination:  &protocol.LedgerPaginationOptions{Limit: uint(limit)},
 		}
 		txsResponse, err := rpcClient.GetTransactions(ctx, req)
 		if err != nil {
 			return errors.Wrapf(err, "failed to fetch transaction data for ledgers %d->%d",
-				parameters.MinLedger, parameters.MinLedger+limit-1)
+				parameters.Range.First, parameters.Range.First+limit-1)
 		}
 		for _, tx := range txsResponse.Transactions {
-			transactions = append(transactions, TxData{
+			item := TxData{
 				TxHash:  tx.TransactionDetails.TransactionHash,
 				Success: tx.TransactionDetails.Status == TxSuccess,
-			})
+			}
+			if err := writer.WriteItem(item); err != nil {
+				return errors.Wrap(err, "failed to write tx item")
+			}
 		}
 	}
-	if err := flushTransactions(parameters.ExportPath, transactions); err != nil {
-		return errors.Wrapf(err, "failed to flush transactions to %s", parameters.ExportPath)
-	}
-	return nil
-}
 
-func flushTransactions(outPath string, transactions []TxData) error {
-	data, err := json.Marshal(transactions)
-	if err != nil {
-		return errors.Wrap(err, "failed to marshal transaction data")
-	}
-	if err := os.WriteFile(outPath, data, 0o644); err != nil {
-		return errors.Wrapf(err, "failed to write results to %s", outPath)
+	if err := writer.EndArray(); err != nil {
+		return errors.Wrap(err, "failed to end txs array")
 	}
 	return nil
 }
