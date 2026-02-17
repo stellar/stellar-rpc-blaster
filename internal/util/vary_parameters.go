edd945544385d5fae05b6163ad1457e6185c1818
Christian Jonas <christianjonas99@gmail.com>
2026-02-17 01:09:04 -0500

    added functions to vary parameters for representative/accurate requests

---
+++ internal/util/vary_parameters.go
@@ -0,0 +1,78 @@
+package util
+
+import (
+	"math/rand/v2"
+
+	protocol "github.com/stellar/go-stellar-sdk/protocols/rpc"
+)
+
+// returns a random limit between 1 and the provided max limit
+func varyLimit(limitMax uint) uint {
+	return uint(rand.Float64()*float64(limitMax-1) + 1)
+}
+
+// determines whether to use "json" or "xdr" format for transaction requests
+// used by all data-dependent endpoints
+func varyFormat() string {
+	if rand.Float64() < PrJson {
+		return "json"
+	}
+	return "xdr"
+}
+
+// randomly decides whether to include a pagination cursor in the request, to vary request patterns for data-dependent endpoints that paginate
+// used by getEvents, getTransactions, getLedgers
+func varyCursorBasedPagination(limit uint, cursor string) *protocol.LedgerPaginationOptions {
+	pagination := &protocol.LedgerPaginationOptions{
+		Limit: limit,
+	}
+	if rand.Float64() < PrCursor {
+		pagination.Cursor = cursor
+	}
+	return pagination
+}
+
+// used only by getEvents, which has a different pagination struct
+func varyCursorBasedPaginationForEvents(limit uint, cursor string) *protocol.PaginationOptions {
+	pagination := &protocol.PaginationOptions{
+		Limit: limit,
+	}
+	if rand.Float64() < PrCursor {
+		c, err := protocol.ParseCursor(cursor)
+		if err != nil {
+			// if cursor parsing fails, fall back to no cursor rather than erroring out, since this is just for request variation
+			return &protocol.PaginationOptions{Limit: limit}
+		}
+		pagination.Cursor = &c
+	}
+	return pagination
+}
+
+// Chooses key count according to the distribution defined in PrKeyCount
+// 80% of requests have 1 key, 15% have between 2 and 10, and 5% will have between 50 and LedgerKeyLimit=200 keys
+// Used by getLedgerEntries to vary the number of keys requested.
+func varyKeyCount() uint {
+	r := rand.Float64()
+	if r < PrKeyCount[0] {
+		return 1
+	} else if r < PrKeyCount[0]+PrKeyCount[1] {
+		return varyLimit(10 - 1)
+	} else {
+		return varyLimit(LedgerKeyLimit-50) + 50 // between 50 and LedgerKeyLimit
+	}
+}
+
+func varyTxStatus(txSuccess float64) string {
+	r := rand.Float64()
+	if txFound() {
+		if r < txSuccess {
+			return TxSuccess
+		}
+		return TxFailed
+	}
+	return TxNotFound
+}
+
+func txFound() bool {
+	return rand.Float64() < PrTxFound
+}
