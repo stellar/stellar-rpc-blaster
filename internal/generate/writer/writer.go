07e2783d28c3e89379636938f0b924b6f20c21a2
Christian Jonas <christianjonas99@gmail.com>
2026-02-27 19:58:37 -0500

    gave SeedWriter io.WriteCloser embedding, enforced interface conformity

--- internal/generate/writer/writer.go
+++ internal/generate/writer/writer.go
@@ -2,16 +2,23 @@ package writer
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/stellar/stellar-rpc-blaster/internal/generate/seed"
 )
 
+var (
+	_ json.Marshaler = (*SeedWriter)(nil)
+	_ io.WriteCloser = (*SeedWriter)(nil)
+)
+
 // SeedWriter accumulates seed data into an OutputSchema struct,
 // then encodes it as a single ordered JSON object on Flush().
 type SeedWriter struct {
-	file *os.File
+	io.WriteCloser
 	seed.SeedData
 }
 
@@ -21,7 +28,7 @@ func NewSeedWriter(path string) (*SeedWriter, error) {
 		return nil, fmt.Errorf("failed to create seed file %s: %v", path, err)
 	}
 	return &SeedWriter{
-		file: f,
+		WriteCloser: f,
 	}, nil
 }
 
@@ -30,23 +37,16 @@ func (w *SeedWriter) MarshalJSON() ([]byte, error) {
 }
 
 func (w *SeedWriter) Write(bytes []byte) (int, error) {
-	return w.file.Write(bytes)
+	return w.WriteCloser.Write(bytes)
 }
 
-func (w *SeedWriter) Close() (err error) {
-	defer func() {
-		if closeErr := w.file.Close(); closeErr != nil {
-			err = closeErr
-		}
-	}()
-
+func (w *SeedWriter) Close() error {
 	data, err := w.MarshalJSON()
 	if err != nil {
-		return fmt.Errorf("failed to marshal seed data to JSON: %v", err)
+		return errors.Join(fmt.Errorf("failed to marshal seed data to JSON: %v", err), w.WriteCloser.Close())
 	}
-
 	if _, err := w.Write(data); err != nil {
-		return fmt.Errorf("failed to write seed data to file: %v", err)
+		return errors.Join(fmt.Errorf("failed to write seed data to file: %v", err), w.WriteCloser.Close())
 	}
-	return
+	return w.WriteCloser.Close()
 }
